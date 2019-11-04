4cea8ef41392e0570866d22973de60de94587f65
NiklasMoller <niklasxmoller@gmail.com>
2019-11-04 11:58:51 +0100

    Added SocketIO asset developed for WEB-GL build

---
+++ Unity/kreativ-data2/Assets/ColorHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorHandler : MonoBehaviour
+{
+    private Color basicColor = Color.green;
+    private Color hoverColor = Color.red;
+    private Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = basicColor;
+    }
+
+    public void ChangeColor()
+    {
+        renderer.material.color = hoverColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Unity/kreativ-data2/Assets/OnConnectionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySocketIO.Events;
+using UnitySocketIO;
+
+public class OnConnectionHandler : MonoBehaviour
+{
+
+    public SocketIOController io;
+    public ColorHandler myCube;
+
+
+        // Start is called before the first frame update
+    void Start()
+        {
+
+        myCube = GameObject.FindObjectOfType<ColorHandler>();
+
+        io.On("connect", (SocketIOEvent e) =>
+            {
+                Debug.Log("SocketIO connected");
+            });
+
+            io.On("OSC", (SocketIOEvent e) =>
+            {
+                Debug.Log("Receievd OSC!");
+                myCube.ChangeColor();
+             
+            });
+
+        io.Connect();
+
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
