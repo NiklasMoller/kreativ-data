1deb52781c23b9182217a3647c8d111fae6fcccb
NiklasMoller <niklasxmoller@gmail.com>
2020-01-28 16:29:07 +0100

    Added camera, scene and animation-loop to template

---
+++ receiveMQTT-max/test.js
@@ -0,0 +1,31 @@
+const maxApi = require("max-api");
+const mqtt = require('mqtt')
+
+//For MQTT
+var options = {qos: 1, keepalive:0,reconnectPeriod:5000}
+
+maxApi.post("The node script was started");
+
+//Free mqtt broker
+var client  = mqtt.connect('mqtt://broker.hivemq.com:1883 ',{
+});
+
+//Called when client is connected
+client.on('connect', function () {
+    maxApi.post('Status: Subscriber is connected to broker');
+
+    client.subscribe('kreativData/#', function (err) {
+        if (!err) {
+            maxApi.post(err);
+        }
+    })
+});
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    maxApi.post('Recieved color: ' + message.toString());
+    maxApi.outletBang();
+})
+
+
